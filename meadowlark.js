90e9418daa014661624a1bacb22695f24ae06055
Louis Ciampanelli <louisciampanelli@Louiss-MBP.home>
2017-07-07 20:01:44 -0400

    added testing

--- meadowlark.js
+++ meadowlark.js
@@ -6,33 +6,61 @@ var fortune = require('./lib/fortune.js');
 // create app
 var app = express();
 // static files
+/*jslint nomen: true*/
 app.use(express.static(__dirname + '/public'));
+/*jslint nomen: false*/
 
 // set up handlebars view engine
-var handelbars = require('express-handlebars')
-    .create({defaultLayout: 'main'});
-app.engine('handlebars', handelbars.engine);
+var handlebars = require('express-handlebars').create({
+    defaultLayout: 'main',
+    helpers: {
+        section: function (name, options) {
+            "use strict";
+            /*jslint nomen: true */
+            if (!this._sections) {
+                this._sections = {};
+            }
+            this._sections[name] = options.fn(this);
+            /*jslint nomen: false */
+            return null;
+        }
+    }
+});
+app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 // set the port
 app.set('port', process.env.PORT || 3000);
 
 
-// testing middlewear
-app.use(function(req, res, next){
+// testing middlewear - shows test with query string '?test=1'
+app.use(function (req, res, next){
     res.locals.showTests = app.get('env') !== 'production' && 
         req.query.test === '1';
     next();
 });
 
+// disable header system information
+app.disable('x-powered-by');
+
 // routes:
 //index page
-app.get('/', function(req, res){
+app.get('/', function (req, res){
     res.render('home');
 });
-// about page
-app.get('/about', function(req, res){
-    res.render('about', {fortune: fortune.getFortune() });
+// about page with test
+app.get('/about', function (req, res){
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: "/qa/tests-about.js"
+    } );
+});
+// request headers 
+app.get('/headers', function(req, res){
+    res.set('Content-Type', 'text/plain');
+    var s = '';
+    for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+    res.send(s);
 });
 
 //catch-all middlewear
@@ -43,6 +71,7 @@ app.use(function(req, res, next){
 });
 // 500 error
 app.use(function(err, req, res, next){
+    "use strict";
     console.log(err.stack);
     res.status(500);
     res.render('500');
@@ -51,4 +80,4 @@ app.use(function(err, req, res, next){
 // start server
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-});
+}); 
